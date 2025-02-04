0d2c2b99cd2cae0abd16d2ad8cb3776f1c4dca48
Pablo Rocamora <pablorocamora@gmail.com>
2025-02-04 19:27:09 +0100

    funciona todo menos paginador y claro o oscuro

---
+++ src/data/projects.js
@@ -0,0 +1,50 @@
+export const projects = {
+  'desarrollo-software-teletrabajo': {
+    title: 'Desarrollo de Software y Teletrabajo',
+    description: 'Un proyecto que explora las mejores prácticas y herramientas para el desarrollo de software en un entorno de teletrabajo.',
+    details: [
+      'Uso de plataformas de colaboración como Slack y Microsoft Teams.',
+      'Gestión de proyectos con herramientas como Jira y Trello.',
+      'Implementación de metodologías ágiles como Scrum y Kanban.',
+      'Desarrollo de software utilizando tecnologías modernas como React y Node.js.',
+      'Integración de herramientas de CI/CD para automatizar el proceso de despliegue.',
+      'Mantenimiento de la calidad del código mediante revisiones de código y pruebas automatizadas.'
+    ]
+  },
+  'automatizacion-tareas-scripts': {
+    title: 'Automatización de Tareas con Scripts',
+    description: 'Este proyecto se centra en la creación de scripts para automatizar tareas repetitivas.',
+    details: [
+      'Automatización de tareas con Python y Bash.',
+      'Reducción del error humano y mejora de la eficiencia.',
+      'Implementación de scripts en entornos de producción.',
+      'Creación de scripts para la gestión de bases de datos y servidores.',
+      'Automatización de procesos de backup y recuperación de datos.',
+      'Desarrollo de scripts para la monitorización y alerta de sistemas.'
+    ]
+  },
+  'despliegue-aplicaciones-nube': {
+    title: 'Despliegue de Aplicaciones en la Nube',
+    description: 'Un proyecto que aborda el despliegue de aplicaciones web en plataformas de nube como AWS y Azure.',
+    details: [
+      'Configuración de servidores y bases de datos.',
+      'Uso de servicios de escalabilidad y balanceo de carga.',
+      'Implementación de CI/CD para despliegue continuo.',
+      'Despliegue de aplicaciones utilizando contenedores Docker.',
+      'Gestión de infraestructura como código con Terraform.',
+      'Monitorización y optimización del rendimiento de las aplicaciones en la nube.'
+    ]
+  },
+  'seguridad': {
+    title: 'Implementación de Seguridad en Aplicaciones Web',
+    description: 'Este proyecto se enfoca en las mejores prácticas para asegurar aplicaciones web.',
+    details: [
+      'Protección contra ataques comunes como XSS, CSRF y SQL Injection.',
+      'Implementación de autenticación y autorización seguras.',
+      'Monitoreo y respuesta a incidentes de seguridad.',
+      'Uso de herramientas de análisis de vulnerabilidades para identificar y mitigar riesgos.',
+      'Implementación de políticas de seguridad y cumplimiento de normativas.',
+      'Formación y concienciación sobre seguridad para los desarrolladores.'
+    ]
+  }
+};
